c5e5885c37227257eadac001e4eaed1b2541aa20
Arturo López <jhonas29@gmail.com>
2024-11-27 12:05:11 +0100

    App.tsx content modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import "./App.css";
+import { useState } from "react";
+import styles from "./App.module.scss";
+import LoadingContainerHero from "./components/loadingContainers/loadingContainerHero/LoadingContainerHero";
+import LetterTranslator from "./components/letterTranslator/LetterTranslator";
+
+type MainStateType = "loading1" | "loading2";
 
 function App() {
-  return <></>;
+  const [mainState, setMainState] = useState<MainStateType>("loading1");
+
+  return (
+    <main className={styles.mainTag}>
+      <header>
+        <h1>
+          <LoadingContainerHero />
+          <span>
+            {mainState !== "loading1" && (
+              <LetterTranslator text="Front-End Developer" activate={true} />
+            )}
+          </span>
+        </h1>
+      </header>
+    </main>
+  );
 }
 
 export default App;
