84b2f1bcb207207e97792583a470eb961baa36fc
Arturo López <jhonas29@gmail.com>
2024-12-03 19:05:59 +0100

    MainNav component created, unfinished

---
+++ src/components/mainNav/MainNav.tsx
@@ -0,0 +1,15 @@
+import styles from "./MainNav.module.scss";
+
+function MainNav() {
+  return (
+    <nav className={styles.mainNavMainContainer}>
+      <ul>
+        <ol></ol>
+        <ol></ol>
+        <ol></ol>
+      </ul>
+    </nav>
+  );
+}
+
+export default MainNav;
