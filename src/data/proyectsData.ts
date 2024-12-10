849bfc6541a3f99ea08a21aca746fed25528f6c2
Arturo López <jhonas29@gmail.com>
2024-12-10 12:22:08 +0100

    proyectsData file created

---
+++ src/data/proyectsData.ts
@@ -0,0 +1,40 @@
+const proyectsNames: string[] = [
+  "Checkmate",
+  "L&R Ingenieros",
+  "Plant In",
+  "EatsQuality",
+  "En 256 Colores",
+];
+
+const proyectsRoutes: string[] = [
+  "/checkmate",
+  "/lringenieros",
+  "/plantin",
+  "/eatsquality",
+  "/en256colores",
+];
+
+const proyectsImagesUrl: string[] = [];
+
+const proyectCheckmateComments: string[] = [];
+
+const proyectLringenierosComments: string[] = [];
+
+const proyectPlantinComments: string[] = [];
+
+const proyectEatsqualityComments: string[] = [];
+
+const proyectEn256coloresComments: string[] = [];
+
+const proyectsData = {
+  proyectsNames: proyectsNames,
+  proyectsRoutes: proyectsRoutes,
+  proyectsImagesUrl: proyectsImagesUrl,
+  proyectCheckmateComments: proyectCheckmateComments,
+  proyectLringenierosComments: proyectLringenierosComments,
+  proyectPlantinComments: proyectPlantinComments,
+  proyectEatsqualityComments: proyectEatsqualityComments,
+  proyectEn256coloresComments: proyectEn256coloresComments,
+};
+
+export default proyectsData;
