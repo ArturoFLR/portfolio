2efef168f923fc1700925ab393e3e373b2897a46
Arturo López <jhonas29@gmail.com>
2024-12-11 20:05:25 +0100

    SliderSmall component working but unfinished

--- src/components/proyectDetails/sliderSmall/SliderSmall.tsx
+++ src/components/proyectDetails/sliderSmall/SliderSmall.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import styles from "./SliderSmall.module.scss";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
@@ -19,6 +19,11 @@ function SliderSmall({
     window.innerWidth > 600 ? false : true,
   );
 
+  const sliderContainerElement = useRef<HTMLDivElement>(null);
+  const prevImgTimeout = useRef<number>(0);
+  const nextImgTimeout = useRef<number>(0);
+
+  //This part defines in which index of the array the images that will be on both sides of the displayed image should be searched.
   let previousImgIndex: number = 0;
   let nextImgIndex: number = 0;
 
@@ -33,29 +38,40 @@ function SliderSmall({
   } else {
     nextImgIndex = shownImageIndex + 1;
   }
+  //END
 
   function handlePrevImgClick() {
-    let newIndex: number;
+    sliderContainerElement.current?.classList.remove(styles.prevImageAnimation);
+    sliderContainerElement.current?.classList.remove(styles.nextImageAnimation);
+    sliderContainerElement.current?.classList.add(styles.prevImageAnimation);
 
-    if (shownImageIndex === 0) {
-      newIndex = imagesList.length - 1;
-    } else {
-      newIndex = shownImageIndex - 1;
-    }
-    console.log(newIndex);
-    setShownImageIndex(newIndex);
+    prevImgTimeout.current = window.setTimeout(() => {
+      let newIndex: number;
+
+      if (shownImageIndex === 0) {
+        newIndex = imagesList.length - 1;
+      } else {
+        newIndex = shownImageIndex - 1;
+      }
+      setShownImageIndex(newIndex);
+    }, 1250);
   }
 
   function handleNextImgClick() {
-    let newIndex: number;
+    sliderContainerElement.current?.classList.remove(styles.prevImageAnimation);
+    sliderContainerElement.current?.classList.remove(styles.nextImageAnimation);
+    sliderContainerElement.current?.classList.add(styles.nextImageAnimation);
 
-    if (shownImageIndex === imagesList.length - 1) {
-      newIndex = 0;
-    } else {
-      newIndex = shownImageIndex + 1;
-    }
-    console.log(newIndex);
-    setShownImageIndex(newIndex);
+    nextImgTimeout.current = window.setTimeout(() => {
+      let newIndex: number;
+
+      if (shownImageIndex === imagesList.length - 1) {
+        newIndex = 0;
+      } else {
+        newIndex = shownImageIndex + 1;
+      }
+      setShownImageIndex(newIndex);
+    }, 1250);
   }
 
   useEffect(() => {
@@ -71,90 +87,99 @@ function SliderSmall({
 
     return () => {
       window.removeEventListener("resize", checkNewResolution);
+      clearTimeout(prevImgTimeout.current);
+      clearTimeout(nextImgTimeout.current);
     };
   }, [isMobileResolution]);
 
+  useLayoutEffect(() => {
+    sliderContainerElement.current?.classList.remove(styles.prevImageAnimation);
+    sliderContainerElement.current?.classList.remove(styles.nextImageAnimation);
+  });
+
   return (
     <div className={styles.sliderMainFrame}>
-      <div className={styles.allImagesContainer}>
-        <div className={styles.prevImageMainContainer}>
-          <button
-            type="button"
-            className={styles.allButtons}
-            aria-label="Ver imagen a pantalla completa"
-          >
-            <img
-              className={styles.allImgTags}
-              src={
-                isMobileResolution
-                  ? imagesMobileList[previousImgIndex]
-                  : imagesList[previousImgIndex]
-              }
-              alt="Captura del proyecto"
-            />
-          </button>
+      <div className={styles.sliderMainContainer}>
+        <div className={styles.allImagesContainer} ref={sliderContainerElement}>
+          <div className={styles.prevImageMainContainer}>
+            <button
+              type="button"
+              className={styles.allButtons}
+              aria-label="Ver imagen a pantalla completa"
+            >
+              <img
+                className={styles.allImgTags}
+                src={
+                  isMobileResolution
+                    ? imagesMobileList[previousImgIndex]
+                    : imagesList[previousImgIndex]
+                }
+                alt="Captura del proyecto"
+              />
+            </button>
+          </div>
+
+          <div className={styles.shownImageMainContainer}>
+            <button
+              type="button"
+              className={styles.allButtons}
+              aria-label="Ver imagen a pantalla completa"
+            >
+              <img
+                className={styles.allImgTags}
+                src={
+                  isMobileResolution
+                    ? imagesMobileList[shownImageIndex]
+                    : imagesList[shownImageIndex]
+                }
+                alt="Captura del proyecto"
+              />
+            </button>
+          </div>
+
+          <div className={styles.nextImageMainContainer}>
+            <button
+              type="button"
+              className={styles.allButtons}
+              aria-label="Ver imagen a pantalla completa"
+            >
+              <img
+                className={styles.allImgTags}
+                src={
+                  isMobileResolution
+                    ? imagesMobileList[nextImgIndex]
+                    : imagesList[nextImgIndex]
+                }
+                alt="Captura del proyecto"
+              />
+            </button>
+          </div>
         </div>
 
-        <div className={styles.shownImageMainContainer}>
-          <button
-            type="button"
-            className={styles.allButtons}
-            aria-label="Ver imagen a pantalla completa"
-          >
-            <img
-              className={styles.allImgTags}
-              src={
-                isMobileResolution
-                  ? imagesMobileList[shownImageIndex]
-                  : imagesList[shownImageIndex]
-              }
-              alt="Captura del proyecto"
-            />
-          </button>
-        </div>
+        <button
+          type="button"
+          onClick={handlePrevImgClick}
+          className={styles.prevImgIcon}
+        >
+          <ArrowLeftIcon
+            color="inherit"
+            fontSize="inherit"
+            style={{ display: "block" }}
+          />
+        </button>
 
-        <div className={styles.nextImageMainContainer}>
-          <button
-            type="button"
-            className={styles.allButtons}
-            aria-label="Ver imagen a pantalla completa"
-          >
-            <img
-              className={styles.allImgTags}
-              src={
-                isMobileResolution
-                  ? imagesMobileList[nextImgIndex]
-                  : imagesList[nextImgIndex]
-              }
-              alt="Captura del proyecto"
-            />
-          </button>
-        </div>
+        <button
+          type="button"
+          onClick={handleNextImgClick}
+          className={styles.nextImgIcon}
+        >
+          <ArrowRightIcon
+            color="inherit"
+            fontSize="inherit"
+            style={{ display: "block" }}
+          />
+        </button>
       </div>
-
-      <button
-        type="button"
-        onClick={handlePrevImgClick}
-        className={styles.prevImgIcon}
-      >
-        <ArrowLeftIcon
-          color="inherit"
-          fontSize="inherit"
-          style={{ display: "block" }}
-        />
-      </button>
-
-      <button
-        type="button"
-        onClick={handleNextImgClick}
-        className={styles.nextImgIcon}
-      >
-        <ArrowRightIcon
-          color="inherit"
-          fontSize="inherit"
-          style={{ display: "block" }}
-        />
-      </button>
     </div>
   );
 }
